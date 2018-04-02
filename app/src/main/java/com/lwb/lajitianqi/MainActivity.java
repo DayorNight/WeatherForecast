045eb15752918b033473b5fe34314ee5744e9a60
BLCSS <2302940971@qq.com>
2018-04-02 18:08:47 +0800

    1、标题

--- app/src/main/java/com/lwb/lajitianqi/MainActivity.java
+++ app/src/main/java/com/lwb/lajitianqi/MainActivity.java
@@ -2,10 +2,51 @@ package com.lwb.lajitianqi;
 
 import android.app.Activity;
 import android.os.Bundle;
-public class MainActivity extends Activity {
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initMenu();
+
     }
+
+    private void initMenu() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //设置Logo
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("");
+        //侧边栏的按钮
+        //菜单点击事件
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Settings !", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
